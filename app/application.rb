edb116897e5c2f216cd8c51b807b506920771a7c
Laura Felce <crepu83@yahoo.it>
2019-03-08 13:58:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,11 @@ class Application
       item_name = req.path.split("/item/").last
       if item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-      end
+      else
+      resp.write "Item not found"
+      resp.status = 400
+    end  
+      
     else
       resp.write "Route not found"
       resp.status = 404
